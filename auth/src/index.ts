0ea3931d8e1165da9909b321413eb7d5071160f6
joao modesto <jvictorgmodesto@gmail.com>
2023-08-28 16:56:10 -0300

    skaffold

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,10 @@ const port = 3000;
 
 app.use(json());
 
+app.get("/api/users/currentuser", (req: any, res: any) => {
+  res.send("Hi there!");
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
