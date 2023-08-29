48f55350d21ad9436f042f9da9cf0c6a0e4eeb51
joao modesto <jvictorgmodesto@gmail.com>
2023-08-29 09:00:51 -0300

    refatorando codigo

--- auth/routes/signUp.ts
+++ auth/routes/signUp.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { body } from "express-validator";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
@@ -12,9 +12,16 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
   ],
-
   (req: Request, res: Response) => {
-    res.send("Hi there!");
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    res.status(201).json({ message: "User created successfully" });
   }
 );
 
